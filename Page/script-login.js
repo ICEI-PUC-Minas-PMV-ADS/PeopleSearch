f78d534686a641f034ac4cc4ddbac517c8c6db88
alemagalhaes01 <alexandremrads@gmail.com>
2022-12-11 13:39:54 -0300

    atualização das paginas login e cadastro

--- Page/script-login.js
+++ Page/script-login.js
@@ -1,1 +1,11 @@
+function Entrar(){
 
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
+    if(email == "admin" && password == "admin"){
+        alert('Sucesso');
+    }else{
+        alert('Usuário ou senha inválido');
+    }
+}
